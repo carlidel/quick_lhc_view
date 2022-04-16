5cd97ab2686f875731af6c4dc11bf5c5893ae26f
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-04-16 16:45:02 +0200

    lhc adaptation

--- figure_gatherer.py
+++ figure_gatherer.py
@@ -1,6 +1,7 @@
 import os
 
-PATH = "/eos/user/c/camontan/definitive_dyn_indicators/figs"
+PATH = "/eos/user/c/camontan/definitive_dyn_indicators/lhc_figs"
+
 
 def check_file_path(path: str) -> str:
     # check if the file exists
@@ -11,34 +12,41 @@ def check_file_path(path: str) -> str:
         raise ValueError(f"File {path} does not exist")
     return path
 
+
 def path_gatherer(
-    omegas: str,
-    epsilon: str,
-    mu: str,
-    plot_kind: str,
-    dynamic_indicator: str,
-    zoom=None,
-    kernel=None,
+    lhc: str, zeta: str, plot_kind: str, dynamic_indicator: str, zoom=None, kernel=None,
 ) -> str:
-    subpath = f"{omegas}/{epsilon}_{mu}" if kernel == "none" or plot_kind == "convolution_performance" else f"{omegas}/{epsilon}_{mu}/{kernel}"
+    subpath = (
+        f"{lhc}/{zeta}"
+        if kernel == "none" or plot_kind == "convolution_performance"
+        else f"{lhc}/{zeta}/{kernel}"
+    )
     print(subpath)
-    
+
     if plot_kind == "performance":
-        out_path = os.path.join(PATH, subpath, f"performance_{dynamic_indicator}_z_{zoom}.jpg")
+        out_path = os.path.join(
+            PATH, subpath, f"performance_{dynamic_indicator}_z_{zoom}.jpg"
+        )
 
     elif plot_kind == "convolution_performance":
-        out_path = os.path.join(PATH, subpath, f"convolution_performance_{dynamic_indicator}_z_{zoom}.jpg")
-    
+        out_path = os.path.join(
+            PATH, subpath, f"convolution_performance_{dynamic_indicator}_z_{zoom}.jpg"
+        )
+
     elif plot_kind == "a_posteriori_comparison":
-        out_path = os.path.join(PATH, subpath, f"all_a_posteriori_performance__z_{zoom}.jpg")
+        out_path = os.path.join(
+            PATH, subpath, f"all_a_posteriori_performance__z_{zoom}.jpg"
+        )
 
     elif plot_kind == "a_priori_comparison":
-        out_path = os.path.join(PATH, subpath, f"all_a_priori_performance__z_{zoom}.jpg")
+        out_path = os.path.join(
+            PATH, subpath, f"all_a_priori_performance__z_{zoom}.jpg"
+        )
 
     elif plot_kind == "histogram":
         out_path = os.path.join(PATH, subpath, f"{dynamic_indicator}_histogram.jpg")
 
-    else:# plot_kind == "colormap":
+    else:  # plot_kind == "colormap":
         out_path = os.path.join(PATH, subpath, "colormap/stable_colormaps.jpg")
 
     return check_file_path(out_path)
--- wsgi.py
+++ wsgi.py
@@ -1,230 +1,257 @@
 # import plotly and dash
 import dash
-from dash import dcc
-from dash import html
 import dash_bootstrap_components as dbc
-from flask_caching import Cache
-from dash.dependencies import Input, Output, State, MATCH, ALL
 import plotly.graph_objects as go
-
-# import pillow
+from dash import dcc, html
+from dash.dependencies import ALL, MATCH, Input, Output, State
 from PIL import Image
-
-app = dash.Dash(__name__, external_stylesheets=[
-                dbc.themes.BOOTSTRAP])
-application = app.server
-
-cache = Cache(app.server, config={
-    'CACHE_TYPE': 'SimpleCache',
-})
-# For security... let's just clean the cache every 48 hours.
-CACHE_TIMEOUT = 48 * 60 * 60
-
-###############################################################################
-
-OMEGA_LIST = [
-    {'label': 'Om_x=0.31 Om_y=0.32', 'value': '0.31_0.32'},
-    {'label': 'Om_x=0.28 Om_y=0.31', 'value': '0.28_0.31'},
-    {'label': 'Om_x=0.168 Om_y=0.201', 'value': '0.168_0.201'},
-]
-
-EPSILON_LIST = [
-    {'label': '0.0', 'value': '0.0'},
-    {'label': '16.0', 'value': '16.0'},
-    {'label': '64.0', 'value': '64.0'},
-    {'label': '128.0' , 'value': '128.0'},
-]
-
-MU_LIST = [
-    {'label': '0.0', 'value': '0.0'},
-    {'label': '0.001', 'value': '0.001'},
-    {'label': '0.01', 'value': '0.01'},
-    {'label': '0.1', 'value': '0.1'}
-]
-
-KERNEL_LIST = [
-    {'label': 'No conv', 'value': 'none'},
-    {'label': '3', 'value': '3'},
-    {'label': '5', 'value': '5'},
-]
-
-DYNAMIC_LIST = [
-    {'label': 'Ground Truth', 'value': "ground_truth"},
-    {'label': 'Fast Lyapunov Indicator', 'value': "fli"},
-    {'label': 'Ortho Lyapunov avg', 'value': "ofli_avg"},
-    {'label': 'Ortho Lyapunov max', 'value': "ofli_max"},
-    {'label': 'Reversibility Error', 'value': "rem"},
-    {'label': 'SALI', 'value': "sali"},
-    {'label': 'GALI', 'value': "gali"},
-    {'label': 'Tune', 'value': "tune"},
-]
-
-PLOT_KIND_LIST = [
-    {'label': 'Colormap', 'value': "colormap"},
-    {'label': 'Performance', 'value': "performance"},
-    {'label': 'Convolution Performance', 'value': "convolution_performance"},
-    {'label': 'All A Posteriori performances', 'value': "a_posteriori_comparison"},
-    {'label': 'All A Priori performances', 'value': "a_priori_comparison"},
-    {'label': 'Histogram', 'value': "histogram"},
-]
-
-ZOOM_LIST = [
-    {'label': '[0.00 - 1.0]', 'value': '0.0'},
-    {'label': '[0.25 - 1.0]', 'value': '0.25'},
-    {'label': '[0.50 - 1.0]', 'value': '0.5'},
-    {'label': '[0.75 - 1.0]', 'value': '0.75'},
-    {'label': '[0.90 - 1.0]', 'value': '0.9'},
-    {'label': '[0.95 - 1.0]', 'value': '0.95'},
-    {'label': '[0.99 - 1.0]', 'value': '0.99'},
-]
-
-picker_1 = [dcc.Dropdown(
-    id={'type': 'picker_1', 'index': i},
-    options=OMEGA_LIST,
-    value=OMEGA_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_2 = [dcc.Dropdown(
-    id={'type': 'picker_2', 'index': i},
-    options=EPSILON_LIST,
-    value=EPSILON_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_3 = [dcc.Dropdown(
-    id={'type': 'picker_3', 'index': i},
-    options=MU_LIST,
-    value=MU_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_4 = [dcc.Dropdown(
-    id={'type': 'picker_4', 'index': i},
-    options=KERNEL_LIST,
-    value=KERNEL_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_5 = [dcc.Dropdown(
-    id={'type': 'picker_5', 'index': i},
-    options=DYNAMIC_LIST,
-    value=DYNAMIC_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_6 = [dcc.Dropdown(
-    id={'type': 'picker_6', 'index': i},
-    options=PLOT_KIND_LIST,
-    value=PLOT_KIND_LIST[0]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-picker_7 = [dcc.Dropdown(
-    id={'type': 'picker_7', 'index': i},
-    options=ZOOM_LIST,
-    value=ZOOM_LIST[3]['value'],
-    clearable=False,
-    multi=False
-) for i in range(6)]
-
-figures = [dcc.Loading(
-    id=f"loading-{i}",
-    type="default",
-    children=dcc.Graph(
-    id={'type': 'figure', 'index': i},
-    figure={'data': [], 'layout': {}}
-    )
-) for i in range(6)]
-
-blocks = [
-    dbc.Col([
-        dbc.Row([
-        dbc.Col([
-            #dbc.FormGroup([
-                dbc.Label("Omega Picker"),
-                picker_1[i],
-                dbc.Label("Epsilon Picker"),
-                picker_2[i],
-                dbc.Label("Mu Picker"),
-                picker_3[i],
-                dbc.Label("Kernel Picker"),
-                picker_4[i],
-                dbc.Label("Dynamic Picker"),
-                picker_5[i],
-                dbc.Label("Plot Kind Picker"),
-                picker_6[i],
-                dbc.Label("Zoom level Picker\n(for performance plots)"),
-                picker_7[i],
-            #]),
-        ], width=2),
-        dbc.Col([
-            figures[i]
-        ])
-        ])
-    ])
-for i in range(6)]
-
-app.layout = html.Div([
-    dbc.Col([
-        dbc.Row([
-            dbc.Col([
-                dbc.Label("Zoom scale:"),
-                dcc.Slider(min=0.05, max=2.0, step=0.05, value=0.5, id='Scale', tooltip={
-                           "placement": "bottom", "always_visible": True})
-            ])
-        ]),
-        dbc.Row([
-            blocks[0],
-            blocks[1],
-        ]),
-        dbc.Row([
-            blocks[2],
-            blocks[3],
-        ]),
-        dbc.Row([
-            blocks[4],
-            blocks[5],
-        ])
-    ]), 
-])
-
-###############################################################################
 
 from figure_gatherer import path_gatherer
 
-# @cache.memoize(timeout=CACHE_TIMEOUT)
-@app.callback(
-    Output({'type': 'figure', 'index': MATCH}, 'figure'),
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+application = app.server
+
+###############################################################################
+
+LHC_LIST = [
+    {"label": "Beam 1, worst seed", "value": "0"},
+    {"label": "Beam 1, average seed", "value": "1"},
+    {"label": "Beam 1, best seed", "value": "2"},
+    {"label": "Beam 2, worst seed", "value": "3"},
+    {"label": "Beam 2, average seed", "value": "4"},
+    {"label": "Beam 2, best seed", "value": "5"},
+]
+
+ZETA_LIST = [
+    {"label": "center", "value": "0"},
+    {"label": "halfway", "value": "1"},
+    {"label": "border", "value": "2"},
+]
+
+KERNEL_LIST = [
+    {"label": "No conv", "value": "none"},
+    {"label": "3", "value": "3"},
+    {"label": "5", "value": "5"},
+]
+
+DYNAMIC_LIST = [
+    {"label": "Ground Truth", "value": "ground_truth"},
+    {"label": "Fast Lyapunov Indicator", "value": "fli"},
+    {"label": "Ortho Lyapunov avg", "value": "ofli_avg"},
+    {"label": "Ortho Lyapunov max", "value": "ofli_max"},
+    {"label": "Reversibility Error", "value": "rem"},
+    {"label": "SALI", "value": "sali"},
+    {"label": "GALI", "value": "gali"},
+    {"label": "Tune", "value": "tune"},
+]
+
+PLOT_KIND_LIST = [
+    {"label": "Colormap", "value": "colormap"},
+    {"label": "Performance", "value": "performance"},
+    {"label": "Convolution Performance", "value": "convolution_performance"},
+    {"label": "All A Posteriori performances", "value": "a_posteriori_comparison"},
+    {"label": "All A Priori performances", "value": "a_priori_comparison"},
+    {"label": "Histogram", "value": "histogram"},
+]
+
+ZOOM_LIST = [
+    {"label": "[0.00 - 1.0]", "value": "0.0"},
+    {"label": "[0.25 - 1.0]", "value": "0.25"},
+    {"label": "[0.50 - 1.0]", "value": "0.5"},
+    {"label": "[0.75 - 1.0]", "value": "0.75"},
+    {"label": "[0.90 - 1.0]", "value": "0.9"},
+    {"label": "[0.95 - 1.0]", "value": "0.95"},
+    {"label": "[0.99 - 1.0]", "value": "0.99"},
+]
+
+# picker_1 = [
+#     dcc.Dropdown(
+#         id={"type": "picker_1", "index": i},
+#         options=OMEGA_LIST,
+#         value=OMEGA_LIST[0]["value"],
+#         clearable=False,
+#         multi=False,
+#     )
+#     for i in range(6)
+# ]
+
+picker_2 = [
+    dcc.Dropdown(
+        id={"type": "picker_2", "index": i},
+        options=LHC_LIST,
+        value=LHC_LIST[0]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+picker_3 = [
+    dcc.Dropdown(
+        id={"type": "picker_3", "index": i},
+        options=ZETA_LIST,
+        value=ZETA_LIST[0]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+picker_4 = [
+    dcc.Dropdown(
+        id={"type": "picker_4", "index": i},
+        options=KERNEL_LIST,
+        value=KERNEL_LIST[0]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+picker_5 = [
+    dcc.Dropdown(
+        id={"type": "picker_5", "index": i},
+        options=DYNAMIC_LIST,
+        value=DYNAMIC_LIST[0]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+picker_6 = [
+    dcc.Dropdown(
+        id={"type": "picker_6", "index": i},
+        options=PLOT_KIND_LIST,
+        value=PLOT_KIND_LIST[0]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+picker_7 = [
+    dcc.Dropdown(
+        id={"type": "picker_7", "index": i},
+        options=ZOOM_LIST,
+        value=ZOOM_LIST[3]["value"],
+        clearable=False,
+        multi=False,
+    )
+    for i in range(6)
+]
+
+figures = [
+    dcc.Loading(
+        id=f"loading-{i}",
+        type="default",
+        children=dcc.Graph(
+            id={"type": "figure", "index": i}, figure={"data": [], "layout": {}}
+        ),
+    )
+    for i in range(6)
+]
+
+blocks = [
+    dbc.Col(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            # dbc.FormGroup([
+                            # dbc.Label("Omega Picker"),
+                            # picker_1[i],
+                            dbc.Label("LHC Picker"),
+                            picker_2[i],
+                            dbc.Label("Zeta Picker"),
+                            picker_3[i],
+                            dbc.Label("Kernel Picker"),
+                            picker_4[i],
+                            dbc.Label("Dynamic Picker"),
+                            picker_5[i],
+                            dbc.Label("Plot Kind Picker"),
+                            picker_6[i],
+                            dbc.Label("Zoom level Picker\n(for performance plots)"),
+                            picker_7[i],
+                            # ]),
+                        ],
+                        width=2,
+                    ),
+                    dbc.Col([figures[i]]),
+                ]
+            )
+        ]
+    )
+    for i in range(6)
+]
+
+app.layout = html.Div(
     [
-        Input({'type': 'picker_1', 'index': MATCH}, 'value'),
-        Input({'type': 'picker_2', 'index': MATCH}, 'value'),
-        Input({'type': 'picker_3', 'index': MATCH}, 'value'),
-        Input({'type': 'picker_4', 'index': MATCH}, 'value'),
-        Input({'type': 'picker_5', 'index': MATCH}, 'value'),
-        Input({'type': 'picker_6', 'index': MATCH}, 'value'),
-        Input('Scale', 'value'),
-        Input({'type': 'picker_7', 'index': MATCH}, 'value'),
+        dbc.Col(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                dbc.Label("Zoom scale:"),
+                                dcc.Slider(
+                                    min=0.05,
+                                    max=2.0,
+                                    step=0.05,
+                                    value=0.5,
+                                    id="Scale",
+                                    tooltip={
+                                        "placement": "bottom",
+                                        "always_visible": True,
+                                    },
+                                ),
+                            ]
+                        )
+                    ]
+                ),
+                dbc.Row([blocks[0], blocks[1],]),
+                dbc.Row([blocks[2], blocks[3],]),
+                dbc.Row([blocks[4], blocks[5],]),
+            ]
+        ),
     ]
 )
+
+###############################################################################
+
+
+@app.callback(
+    Output({"type": "figure", "index": MATCH}, "figure"),
+    [
+        # Input({"type": "picker_1", "index": MATCH}, "value"),
+        Input({"type": "picker_2", "index": MATCH}, "value"),
+        Input({"type": "picker_3", "index": MATCH}, "value"),
+        Input({"type": "picker_4", "index": MATCH}, "value"),
+        Input({"type": "picker_5", "index": MATCH}, "value"),
+        Input({"type": "picker_6", "index": MATCH}, "value"),
+        Input("Scale", "value"),
+        Input({"type": "picker_7", "index": MATCH}, "value"),
+    ],
+)
 def update_fig(*args):
-    omegas = args[0]
-    epsilon = args[1]
-    mu = args[2]
-    kernel = args[3]
-    dynamic = args[4]
-    plot_kind = args[5]
-    
+    lhc = args[0]
+    zeta = args[1]
+    kernel = args[2]
+    dynamic = args[3]
+    plot_kind = args[4]
+
     fig = go.Figure()
 
     try:
-        figpath = path_gatherer(omegas=omegas, epsilon=epsilon, mu=mu, dynamic_indicator=dynamic, plot_kind=plot_kind, zoom=args[7], kernel=kernel)
+        figpath = path_gatherer(
+            lhc=lhc,
+            zeta=zeta,
+            dynamic_indicator=dynamic,
+            plot_kind=plot_kind,
+            zoom=args[7],
+            kernel=kernel,
+        )
     except Exception:
         return fig
 
@@ -235,7 +262,7 @@ def update_fig(*args):
 
     img_width = 1920
     img_height = 1080
-    
+
     # Add invisible scatter trace.
     # This trace is added to help the autoresize logic work.
     fig.add_trace(
@@ -243,21 +270,18 @@ def update_fig(*args):
             x=[0, img_width * scale_factor],
             y=[0, img_height * scale_factor],
             mode="markers",
-            marker_opacity=0
+            marker_opacity=0,
         )
     )
 
     # Configure axes
-    fig.update_xaxes(
-        visible=False,
-        range=[0, img_width * scale_factor]
-    )
+    fig.update_xaxes(visible=False, range=[0, img_width * scale_factor])
 
     fig.update_yaxes(
         visible=False,
         range=[0, img_height * scale_factor],
         # the scaleanchor attribute ensures that the aspect ratio stays constant
-        scaleanchor="x"
+        scaleanchor="x",
     )
 
     # Add image
@@ -272,7 +296,8 @@ def update_fig(*args):
             opacity=1.0,
             layer="below",
             sizing="contain",
-            source=img)
+            source=img,
+        )
     )
 
     # Configure other layout
@@ -281,11 +306,12 @@ def update_fig(*args):
         height=img_height * scale_factor,
         margin={"l": 0, "r": 0, "t": 0, "b": 0},
     )
-    
+
     return fig
+
 
 ###############################################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Test")
     app.run_server(host="0.0.0.0", port=8080, debug=True)
