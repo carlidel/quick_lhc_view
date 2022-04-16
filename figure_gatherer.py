2f7d60d7d562c982346326acbb3979d59740e0af
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-04-16 14:32:47 +0200

    initial commit

---
+++ figure_gatherer.py
@@ -0,0 +1,44 @@
+import os
+
+PATH = "/eos/user/c/camontan/definitive_dyn_indicators/figs"
+
+def check_file_path(path: str) -> str:
+    # check if the file exists
+    # if not, raise a ValueError
+    print(path)
+    if not os.path.exists(path):
+        print("File does not exist")
+        raise ValueError(f"File {path} does not exist")
+    return path
+
+def path_gatherer(
+    omegas: str,
+    epsilon: str,
+    mu: str,
+    plot_kind: str,
+    dynamic_indicator: str,
+    zoom=None,
+    kernel=None,
+) -> str:
+    subpath = f"{omegas}/{epsilon}_{mu}" if kernel == "none" or plot_kind == "convolution_performance" else f"{omegas}/{epsilon}_{mu}/{kernel}"
+    print(subpath)
+    
+    if plot_kind == "performance":
+        out_path = os.path.join(PATH, subpath, f"performance_{dynamic_indicator}_z_{zoom}.jpg")
+
+    elif plot_kind == "convolution_performance":
+        out_path = os.path.join(PATH, subpath, f"convolution_performance_{dynamic_indicator}_z_{zoom}.jpg")
+    
+    elif plot_kind == "a_posteriori_comparison":
+        out_path = os.path.join(PATH, subpath, f"all_a_posteriori_performance__z_{zoom}.jpg")
+
+    elif plot_kind == "a_priori_comparison":
+        out_path = os.path.join(PATH, subpath, f"all_a_priori_performance__z_{zoom}.jpg")
+
+    elif plot_kind == "histogram":
+        out_path = os.path.join(PATH, subpath, f"{dynamic_indicator}_histogram.jpg")
+
+    else:# plot_kind == "colormap":
+        out_path = os.path.join(PATH, subpath, "colormap/stable_colormaps.jpg")
+
+    return check_file_path(out_path)
